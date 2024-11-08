bdcdc49c00ce0ccd7d4da25ee20257b02076d93c
jakes-johns <jakesjohns29@gmail.com>
2024-11-08 23:47:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.secret_key = os.getenv('SECRET_KEY')  # Replace with a real secret key
 
 # Configure Flask-Mail
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))  # Default to 587 if MAIL_PORT is not set
 app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
